4dd41db28d1445908b24821c0db58044a0605369
kosamson <kosamson1@gmail.com>
2020-12-19 22:44:19 -0800

    Clean up GUI code and UI

--- src/main/java/AppGUI.java
+++ src/main/java/AppGUI.java
@@ -9,52 +9,36 @@ public class AppGUI {
         // a comment
         SBApp app = new SBApp();
 
-        JFrame jf = new JFrame();
-        jf.setSize(1000, 1000);
-        jf.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        JFrame mainFrame = new JFrame();
+        mainFrame.setSize(500, 500);
+        mainFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
-        JTextArea jta = new JTextArea();
-        jta.setBounds(550, 600, 100, 100);
+        JLabel infoDisplay = new JLabel();
+        infoDisplay.setText("Skill Bank Display");
+        infoDisplay.setBounds(50, 0, 300, 200);
 
-        JButton jb2 = new JButton("Add Skill");
-        jb2.setBounds(400, 400, 200, 100);
+        JTextArea addSkillInput = new JTextArea();
+        addSkillInput.setBounds(300, 25, 150, 20);
 
-        jb2.addActionListener(new ActionListener() {
+        JButton addSkillButton = new JButton("Add Skill");
+        addSkillButton.setBounds(300, 50, 100, 25);
+
+        addSkillButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                String label = jta.getText();
+                String label = addSkillInput.getText();
                 app.addSkill(label);
+                infoDisplay.setText(convertToHTML(app.sbInfo()));
             }
         });
 
         // Add placeholder text to text fields
 
-        JLabel jl1 = new JLabel();
-        jl1.setText("Skill Bank Display");
-        jl1.setBounds(50, 50, 400, 600);
+        mainFrame.add(infoDisplay);
+        mainFrame.add(addSkillButton);
+        mainFrame.add(addSkillInput);
 
-        JButton jb = new JButton();
-        jb.setText("Update Display");
-        jb.setBounds(250, 250, 200, 100);
-
-        jb.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                String convertedInfo = convertToHTML(app.sbInfo());
-                jl1.setText(convertedInfo);
-            }
-        });
-
-        jf.add(jb);
-        jf.add(jl1);
-        jf.add(jb2);
-        jf.add(jta);
-
-        jb.setVisible(true);
-        jl1.setVisible(true);
-        jb2.setVisible(true);
-        jta.setVisible(true);
-
-        jf.setLayout(null);
-        jf.setVisible(true);
+        mainFrame.setLayout(null);
+        mainFrame.setVisible(true);
     }
 
     private static String convertToHTML(String s) {
@@ -65,7 +49,6 @@ public class AppGUI {
         sb.append(s.replace(", ", "<br>"));
 
         sb.append("</p></html>");
-        System.out.println(sb.toString());
         return sb.toString();
     }
 }
